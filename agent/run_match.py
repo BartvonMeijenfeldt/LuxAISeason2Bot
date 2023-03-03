66d85fc2aa3e33348a36e280120bd6fed85c2c6f
Bart von Meijenfeldt <b.m.vonmeijenfeldt@gmail.com>
2023-03-03 21:01:00 +0100

    run_match to allow cli nr_steps and seed

--- agent/run_match.py
+++ agent/run_match.py
@@ -1,11 +1,17 @@
 from luxai_s2.env import LuxAI_S2
+from argparse import ArgumentParser
 
 from agent import Agent
 from visualize.interactions import interact
+
+parser = ArgumentParser()
+parser.add_argument('--nr_steps', default=1000, type=int)
+parser.add_argument('--seed', default=212457496, type=int)
+args = parser.parse_args()
 
 env = LuxAI_S2()
 env.reset()
 
 # recreate our agents and run
 agents = {player: Agent(player, env.state.env_cfg) for player in env.agents}
-interact(env, agents, 1000, seed=35)
+interact(env, agents, args.nr_steps, seed=args.seed)
