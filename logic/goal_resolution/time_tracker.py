a729743ebb14017e06fb7b5c04dc1ab1c4847590
Bart von Meijenfeldt <b.m.vonmeijenfeldt@gmail.com>
2023-04-23 07:59:12 +0200

    time_out_protection

---
+++ logic/goal_resolution/time_tracker.py
@@ -0,0 +1,36 @@
+import time
+import logging
+
+from dataclasses import dataclass
+from config import CONFIG
+
+
+@dataclass
+class TimeTracker:
+    start_time: float
+    DEBUG_MODE: bool
+
+    def _get_time_taken(self) -> float:
+        return time.time() - self.start_time
+
+    def is_out_of_time_main_scheduling(self) -> bool:
+        if self.DEBUG_MODE:
+            return False
+
+        is_out_of_time = self._get_time_taken() > CONFIG.OUT_OF_TIME_MAIN_SCHEDULING
+
+        if is_out_of_time:
+            logging.critical("RAN OUT OF TIME MAIN SCHEDULING")
+
+        return is_out_of_time
+
+    def is_out_of_time_scheduling_unassigned_units(self) -> bool:
+        if self.DEBUG_MODE:
+            return False
+
+        is_out_of_time = self._get_time_taken() > CONFIG.OUT_OF_TIME_UNASSIGNED_SCHEDULING
+
+        if is_out_of_time:
+            logging.critical("RAN OUT OF TIME UNASSIGNED SCHEDULING")
+
+        return is_out_of_time
