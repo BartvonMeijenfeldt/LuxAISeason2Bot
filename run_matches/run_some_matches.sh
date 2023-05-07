1c673ec89fb6724bc201bc6f37517ce67b6ff73c
Bart von Meijenfeldt <b.m.vonmeijenfeldt@gmail.com>
2023-05-07 21:47:42 +0200

    fix run some matches

--- run_matches/run_some_matches.sh
+++ run_matches/run_some_matches.sh
@@ -7,6 +7,6 @@ do
     cli_output_file="data/"$counter"_cli_output.txt"
 
     luxai-s2 main.py main.py -s $counter -v 3 -o $replay_file > $cli_output_file
-    python run_some_matches/parse_cli_output.py --cli_output_path=$cli_output_file
+    python run_matches/parse_cli_output.py --cli_output_path=$cli_output_file
     ((counter++))
 done
